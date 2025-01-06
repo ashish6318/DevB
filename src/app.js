7f8daf965edda666c0347b24407744e100767c87
Stoneblader <foul.uncle223@gmail.com>
2025-01-06 21:02:32 +0530

    explored get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const app = express();
 
-// Specific routes first
-app.use("/hello", (req, res) => {
-  res.send("Hello World");
+app.get("/user",(req,res)=>{
+  res.send({
+    "name":"John",
+    "age":30
+  })
 });
+
+app.post("/user",async(req,res)=>{
+  res.send("Data Successfully saved to the database")
+});
+
+app.delete("/user",(req,res)=>{
+  res.send("User deleted successfully");
+});
+
+// Specific routes first
+
 
 app.use("/test", (req, res) => {
   res.send("Hello World we are testing server");
 });
 
 // General route last
-app.use("/", (req, res) => {
-  res.send("Hello from dashboard");
-});
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
